d47518b2db0ab583b4ac7dac521c386b44a9566e
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-26 18:04:24 -0300

    styles for general application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
-import {CTA, Brand, NavBar} from './components';
+import './App.css'
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, NavBar } from './components';
 
 const App = () => {
-  return (
-    <div className='App'>
-    </div>
-  )
+	return (
+		<div className='App'>
+			<div className='gradient__bg'>
+				<NavBar />
+				<Header />
+			</div>
+			<Brand/>
+			<WhatGPT3/>
+			<Features/>
+			<Possibility/>
+			<CTA/>
+			<Blog/>
+			<Footer/>
+		</div>
+	)
 }
 
 export default App
