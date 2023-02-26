7079fc4486e8b86672df428798b8abe1932afe73
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-26 17:42:38 -0300

    imports on app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+import {CTA, Brand, NavBar} from './components';
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
     </div>
   )
 }
