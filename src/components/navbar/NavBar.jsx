38132371d7277566c22ec1e464a29fd591a00bac
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-26 21:53:29 -0300

    assets, navbar jsx structure and style

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+	return (
+		<div className='gpt3__navbar'>
+			<div className='gpt3__navbar-links'>
+				<div className='gpt3__navbar-links_logo'>
+					<img src={logo} alt='logo'></img>
+				</div>
+				<div className='gpt3__navbar-links_container'>
+					<p><a href="#home">Home</a></p>
+					<p><a href="#wgpt3">What is GPT3</a></p>
+					<p><a href="#possibility">Open AI</a></p>
+					<p><a href="#features">Case Studies</a></p>
+					<p><a href="#blog">Libary</a></p>
+				</div>
+			</div>
+			<div className='gpt3__navbar-sign'>
+				<p>Sign In</p>
+				<button type='button'>Sign Up</button>
+			</div>
+		</div>
+	)
 }
 
 export default NavBar
