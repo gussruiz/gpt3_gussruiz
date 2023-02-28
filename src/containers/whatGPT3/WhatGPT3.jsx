9e948ec1d1f924e77f61ee76493e0e2359a14d32
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-28 19:18:29 -0300

    css of brand done

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,15 +4,15 @@ import './whatGPT3.css';
 
 const WhatGPT3 = () => {
 	return (
-		<div className='gpt3__whatpgt3 section__margin' id='whpt3'>
-			<div className='gpt3__whatpgt3-feature'>
+		<div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+			<div className='gpt3__whatgpt3-feature'>
 				<Feature/>
 			</div>
-			<div className='gpt3__whatpgt3-heading'>
-				<h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+			<div className='gpt3__whatgpt3-heading'>
+				<h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
 				<p>Explore The Libary</p>
 			</div>
-			<div className='gpt3__whatpgt3-container'>
+			<div className='gpt3__whatgpt3-container'>
 				<Feature/>
 				<Feature/>
 				<Feature/>
